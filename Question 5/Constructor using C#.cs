421202996eafa0835b8679cb159ced58d67a3678
RedX_Online <118306203+RudraSharma05@users.noreply.github.com>
2024-11-04 12:44:00 +0530

    Add files via upload

---
+++ Question 5/Constructor using C#.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConstructorExample
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+            name = "Unknown";
+            age = 0;
+            Console.WriteLine("Default Constructor Called");
+        }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            Console.WriteLine("Parameterized Constructor Called");
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {name}, Age: {age}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Person person1 = new Person();
+            person1.DisplayInfo();
+
+            Person person2 = new Person("Tomba", 20);
+            person2.DisplayInfo();
+        }
+    }
+}
